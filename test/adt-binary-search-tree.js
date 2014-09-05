51680d208b713021f94ede60caa55b4c025e0998
Ian Anderson <ian@gozumi.com>
2014-09-05 18:53:25 +0100

    More development on the binary search tree
    13 tests passing
    
    13 tests pending

--- test/adt-binary-search-tree.js
+++ test/adt-binary-search-tree.js
@@ -71,7 +71,7 @@ describe.only('adt-binary-search-tree', function () {
 			});
 
 
-			it('should add objects with a \'key\' propperty', function() {
+			it('should insert objects with a \'key\' propperty', function() {
 
 				var myObject = {my: 'object', key: 1};
 				bst.insert(myObject);
@@ -92,17 +92,15 @@ describe.only('adt-binary-search-tree', function () {
 			});
 
 
-			it('should throw an error if the item is an object with a \'key\' property,	and the key property is an object', function() {
+			it('should throw an error if the item is an object with a \'key\' property, and the key property is an object', function() {
 
+				var myObject = {my: 'object', key: {the: 'value'}};
 
+				(function() {
+					bst.insert(myObject);
+				}).should.throw(Error);
 
 			});
-
-
-			it('should return the correct key if the item is an object with a \'key\' property,	and the key property is not an object');
-
-
-			it('should return the correct key if the item is not an object');
 
 
 		});
@@ -129,6 +127,25 @@ describe.only('adt-binary-search-tree', function () {
 
 			it('should insert a greater item as the root of the right sub tree', function() {
 
+				var rootItem = 'orange';
+				bst.insert(rootItem);
+
+				var newItem = 'pear';
+				bst.insert(newItem);
+
+				bst.getRightTree().getRootItem().should.equal(newItem);
+
+			});
+	
+
+		});
+
+
+		describe('when the left sub tree is empty', function () {
+			
+	
+			it('should insert a lesser item as the root of the left sub tree', function() {
+
 				var rootItem = 'pear';
 				bst.insert(rootItem);
 
@@ -143,21 +160,135 @@ describe.only('adt-binary-search-tree', function () {
 		});
 
 
-		describe('when the left sub tree is empty', function () {
+		describe('when perfectly balanced data is given', function () {
 			
 	
-			it('should insert a lesser item as the root of the left sub tree');
+			it('should create a perfectly balanced tree', function() {
+
+				var data = [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15];
+
+				for (var i = 0; i < data.length; i++) {
+					bst.insert(data[i]);
+				}
+
+
+				bst.getRootItem().should.equal(8);
+				bst.getLeftTree().getRootItem().should.equal(4);
+				bst.getRightTree().getRootItem().should.equal(12);
+				bst.getLeftTree()
+					.getLeftTree()
+					.getRootItem().should.equal(2);
+				bst.getLeftTree()
+					.getRightTree()
+					.getRootItem().should.equal(6);
+				bst.getLeftTree()
+					.getLeftTree()
+					.getLeftTree()
+					.getRootItem().should.equal(1);
+				bst.getLeftTree()
+					.getLeftTree()
+					.getRightTree()
+					.getRootItem().should.equal(3);
+				bst.getLeftTree()
+					.getRightTree()
+					.getLeftTree()
+					.getRootItem().should.equal(5);
+				bst.getLeftTree()
+					.getRightTree()
+					.getRightTree()
+					.getRootItem().should.equal(7);
+				bst.getRightTree()
+					.getLeftTree()
+					.getRootItem().should.equal(10);
+				bst.getRightTree()
+					.getRightTree()
+					.getRootItem().should.equal(14);
+				bst.getRightTree()
+					.getLeftTree()
+					.getLeftTree()
+					.getRootItem().should.equal(9);
+				bst.getRightTree()
+					.getLeftTree()
+					.getRightTree()
+					.getRootItem().should.equal(11);
+				bst.getRightTree()
+					.getRightTree()
+					.getLeftTree()
+					.getRootItem().should.equal(13);
+				bst.getRightTree()
+					.getRightTree()
+					.getRightTree()
+					.getRootItem().should.equal(15);
+
+			});
 	
 
 		});
 
 
-		describe('when perfectly balanced data is given', function () {
-			
-	
-			it('should create a perfectly balanced tree');
-	
+		describe('when items are inserted', function () {
 
+
+			it('all sub trees should be of type ADT_BinarySearchTree', function() {
+
+				var data = [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15];
+
+				for (var i = 0; i < data.length; i++) {
+					bst.insert(data[i]);
+				}
+
+
+				bst.getLeftTree().constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getLeftTree().constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getRightTree().constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getLeftTree()
+					.getLeftTree()
+					.constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getLeftTree()
+					.getRightTree()
+					.constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getLeftTree()
+					.getLeftTree()
+					.getLeftTree()
+					.constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getLeftTree()
+					.getLeftTree()
+					.getRightTree()
+					.constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getLeftTree()
+					.getRightTree()
+					.getLeftTree()
+					.constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getLeftTree()
+					.getRightTree()
+					.getRightTree()
+					.constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getRightTree()
+					.getLeftTree()
+					.getRootItem().should.equal(10);
+				bst.getRightTree()
+					.getRightTree()
+					.constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getRightTree()
+					.getLeftTree()
+					.getLeftTree()
+					.constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getRightTree()
+					.getLeftTree()
+					.getRightTree()
+					.constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getRightTree()
+					.getRightTree()
+					.getLeftTree()
+					.constructor.name.should.equal('ADT_BinarySearchTree');
+				bst.getRightTree()
+					.getRightTree()
+					.getRightTree()
+					.constructor.name.should.equal('ADT_BinarySearchTree');
+
+			});
+
+			
 		});
 
 
